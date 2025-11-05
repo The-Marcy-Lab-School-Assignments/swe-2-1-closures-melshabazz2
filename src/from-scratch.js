53df8aa901cf30f16cd6b89f66c5227a760da214
melshabazz2 <melshabazz2@gmail.com>
2025-11-05 15:02:33 -0500

    completed some from scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,21 +1,42 @@
 const makeIdFunc = () => {
-
+  let id = 0
+  return () => {
+    id++
+    return id
+  }
 };
 
 const makePasswordChecker = (correctPassword) => {
-
-};
+  let attempts = 0
+  return (guess) => {
+    if (attempts >= 3) {
+      return "Account locked"
+    } if (guess === correctPassword) {
+      attempts = 0
+      return true
+    } if (attempts >= 3) {
+      return "Account locked"
+    } else {
+      attempts++;
+      return false
+    }
+  }
+}
 
 const makeMultiplier = (multiplier) => {
-
+  return (numbers) => {
+    return numbers.map((number) => number * multiplier)
+  }
 };
 
-const makeFilterByLength = (length) => {
-
+const makeFilterByLength = (maxLength) => {
+  return (strings) => {
+    return strings.filter(string => string.length <= maxLength)
+  }
 };
 
 const makeGradeTracker = () => {
-
+  let grades = 
 };
 
 const makeShoppingList = () => {
